540ffd4c0f1db4a580369a459c565b9464e90f95
Anissoft <anissoftkun@gmail.com>
2020-09-18 19:04:50 +0700

    fixing case problem

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,6 @@
 export const DIVIDER = '::';
-export const SERVICE = 'rSERVICE'; 
+export const SERVICE = 'rSERVICE';
 
 export const PARAMETER_PREFIX = `PARAM${DIVIDER}`;
-export const PROPERTY_PREFIX = `PROP${DIVIDER}`; 
-// export const VISIBILITY_GLOBAL = '__GLOBAL'; 
-// export const VISIBILITY_SCOPED= '__SCOPED'; 
+export const PROPERTY_PREFIX = `PROP${DIVIDER}`;
 export const DEPENDENCIES = 'DEPENDENCIES'; 
